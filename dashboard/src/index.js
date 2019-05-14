fefcba1cb4814ba801f0df8feeffcc9a74ca8563
Sih-Yi Lin <sihyiyl2@illinois.edu>
2019-05-14 18:35:04 -0500

    Change App path

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './example';
-// import App from './components/App/App';
-import App from './App';
+import App from './components/App/App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
