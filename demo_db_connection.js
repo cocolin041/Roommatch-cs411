db3e5725fdace2b9fee4ea01dccc6b148f16fc8c
Sih-Yi Lin <sihyiyl2@illinois.edu>
2019-05-14 17:49:02 -0500

    test deploy

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -12,13 +12,13 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //connect to mysql
-// var con = mysql.createPool({
-//   host: "us-cdbr-iron-east-02.cleardb.net",
-//   database: "heroku_55d31885725360a",
-//   user: "bcb93c414806a9",
-//   password: "e6eea2e6",
-//   connectionLimit : 10
-// });
+var con = mysql.createPool({
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  database: "heroku_b4c2490803a0411",
+  user: "bec78dfb179f6c",
+  password: "3add20d4",
+  connectionLimit : 10
+});
 
 //open the cross access
 app.use(function(req, res, next) {
