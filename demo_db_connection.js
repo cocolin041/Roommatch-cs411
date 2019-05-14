d51487412709ee9482524cb263a11f935dcdceed
Sih-Yi Lin <sihyiyl2@illinois.edu>
2019-05-14 18:26:19 -0500

    Delete multer package

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // var moment = require('moment');
 var mysql = require('mysql');
-var multer = require('multer');
+// var multer = require('multer');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
